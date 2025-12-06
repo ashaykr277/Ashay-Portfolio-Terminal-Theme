999cabbc84374f72ab15b7e9c27b1f53383c41ea
Astro Dev <ashaykr277@gmail.com>
2025-12-07 01:27:49 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <center>
+
+                    <div>
+                        <div className="icons">
+                            <i class='bx bxl-linkedin-square'></i>
+                            <i class='bx bxl-github' ></i>
+                            <i class='bx bx-envelope' ></i>
+                        </div>
+                    </div>
+                    <hr />
+                    <p>copyright &copy; 2025 - Ashay Kumar - All rights reserved </p>
+
+                </center>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  return (
+    <>
+    <header>
+        <div className="logo">
+            <h1>ASHAY_KUMAR.EXE</h1>
+        </div>
+        <nav>
+            <ul className="nav-elements">
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <button className='btn'>Hire Me</button>
+
+    </header>
+    </>
+  )
+}
+
+export default Navbar
