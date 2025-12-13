23af336f39a08cd4745c50331e8021e9cc82a659
Astro Dev <ashaykr277@gmail.com>
2025-12-14 01:36:04 +0530

    added projects final

--- src/components/ProjBox/Projbox.jsx
+++ src/components/ProjBox/Projbox.jsx
@@ -18,37 +18,18 @@ const Projbox = (props) => {
                 </div>
                 <div className="btns">
                     <div className="btn2 code">
-                        <a href="https://github.com/ashaykr277/Ai-Proctored-Major-Project" target="_blank" rel="noopener noreferrer">
+                        <a href={props.href1} target="_blank" rel="noopener noreferrer">
                             <i className="bx bxl-github"></i> Code
                         </a>
                     </div>
 
-                    <div className="btn2 Demo"><a href="" target="_blank" rel="noopener noreferrer">
+                    <div className="btn2 Demo"><a href={props.href2} target="_blank" rel="noopener noreferrer">
                         <i class='bx bx-globe' ></i>Demo
                     </a>
                     </div>
                 </div>
             </div>
         </div>
-
-
-            <div className="edu-box">
-                <div className="top">
-                    <i class={props.icon} ></i>
-                    <div className="institute">{props.skill}</div>
-                </div>
-
-
-                <div className="edu-details">
-                    <div className="degree">{props.degree}</div>
-                    <div className="branch">{props.branch}</div>
-                    <div className="down">
-                        <div className="year"><i class='bx bx-calendar' ></i>{props.yr1}-{props.yr2}</div>
-                        <div className="loc"><i class='bx bx-map' ></i>{props.loc}</div>
-                    </div>
-                </div>
-
-            </div>
         </>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,30 +33,54 @@ const Projects = () => {
                         tech1="Python"
                         tech2="OpenCV"
                         tech3="YOLO"
+                        href1="https://github.com/ashaykr277/Ai-Proctored-Major-Project"
+                        href2=""
+                    />
+                    <Projbox 
+                        src="https://img.freepik.com/premium-vector/face-recognition-scanning-flat-style-illustration_275655-582.jpg?w=1060"
+                        title="Face Recognition System"
+                        desc="it is an AI-powered application designed to identify and verify individuals based on their facial features. It uses computer vision and deep learning techniques to accurately detect, recognize, and match faces from live camera feeds or stored images."
+                        tech1="Python"
+                        tech2="OpenCV"
+                        tech3=""
+                        href1="https://github.com/ashaykr277/Face-Recognition-System"
+                        href2=""
+                    />
+                    <Projbox 
+                        src="https://img.freepik.com/free-vector/digital-marketing-team-constructing-landing-home-page_74855-10590.jpg?t=st=1765655972~exp=1765659572~hmac=ec8d6e01b2e59c8f37408e3227be90dee7d3fba791e9840fa4fd4af400de08c1&w=1480"
+                        title="Portfolio Website"
+                        desc="it is is a responsive and interactive personal website built with React.js to showcase professional skills, and projects. Designed with modern UI/UX approach, serving as a digital identity for personal branding and career opportunities."
+                        tech1="JavaScript"
+                        tech2="CSS"
+                        tech3="React.js"
+                        href1="https://github.com/ashaykr277/Ashay-Portfolio-Terminal-Theme"
+                        href2="https://ashay-portfolio-terminal.vercel.app/"
+                    />
+                    <Projbox 
+                        src="https://img.freepik.com/premium-vector/diverse-group-professionals-engages-cohort-analysis-using-digital-tools-visuals-enhancing-teamwork-customizable-flat-illustration-cohort-analysis_585735-36569.jpg?w=1480"
+                        title="Employee Management System"
+                        desc="it is an advanced online testing platform designed to ensure the integrity and fairness of remote examinations through real-time AI monitoring. with face recognition, object detection, to prevent cheating and maintain exam security without human invigilators."
+                        tech1="Java"
+                        tech2="Postgresql"
+                        tech3="Spring-boot"
+                        href1=""
+                        href2=""
+                    />
+                    
+                    <Projbox 
+                        // src="https://img.freepik.com/free-vector/flat-design-online-ticket-illustration_23-2151089479.jpg?t=st=1765656260~exp=1765659860~hmac=84dc1a829df106d1397ee931950b148c74d550f7727695c4fe62f128f908281e&w=740"
+                        src="https://img.freepik.com/free-vector/tiny-man-buying-ticket-online-via-laptop-monitor-plane-baggage-flat-vector-illustration-travelling-digital-technology_74855-8621.jpg?t=st=1765656318~exp=1765659918~hmac=833455cb9f29048ee7be161527865b02387a129e09819d76a9db32f687b41d60&w=1480"
+                        title="Employee Management System"
+                        desc="it is an advanced online testing platform designed to ensure the integrity and fairness of remote examinations through real-time AI monitoring. with face recognition, object detection, to prevent cheating and maintain exam security without human invigilators."
+                        tech1="Java"
+                        tech2="Postgresql"
+                        tech3="Spring-boot"
+                        href1=""
+                        href2=""
                     />
                     
                     
-                    <Edubox skill="National Institute of Science and Technology(NIST)"
-                        icon="bx bxs-graduation"
-                        degree="Bachelor of Technology"
-                        branch="> : ~\Computer Science Engineering"
-                        yr1="2021" yr2="2025"
-                        loc="Brahmapur, Odisha" />
-
-                    <Edubox skill="Sri Chaitanya Techno School"
-                        icon="bx bxs-school"
-                        degree="Intermediate"
-                        branch="> : ~\Physics Chemistry Maths"
-                        yr1="2018" yr2="2020"
-                        loc="Visakhapatnam, Andhra Pradesh" />
-
-                    <Edubox skill="B.D.M International School"
-                        icon="bx bx-bus-school"
-
-                        degree="Matriculation"
-                        branch="> : ~\CBSE Board"
-                        yr1="2017" yr2="2018"
-                        loc="Kolkata, West Bengal" />
+                    
                 </div>
                 <div className="status">
                     <div className="details">
